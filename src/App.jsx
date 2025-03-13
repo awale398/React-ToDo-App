2a8bb33a53a130e71d457e4982c328ddbd31f4fc
Awale398 <adenabdiawal@gmail.com>
2025-03-13 12:18:18 +0300

    React Todo list app

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import { useState, useEffect } from "react";
+import AddTaskForm from "./components/AddTaskForm.jsx";
+import UpdateForm from "./components/UpdateForm.jsx";
+import ToDo from "./components/ToDo.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  // Load tasks from localStorage or initialize an empty array
+  const [toDo, setToDo] = useState(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+
+  // Save tasks to localStorage whenever they change
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(toDo));
+  }, [toDo]);
+
+  // Temp State
+  const [newTask, setNewTask] = useState("");
+  const [updateData, setUpdateData] = useState("");
+
+  // Add task
+  const addTask = () => {
+    if (newTask) {
+      let newEntry = { id: Date.now(), title: newTask, status: false };
+      setToDo([...toDo, newEntry]);
+      setNewTask("");
+    }
+  };
+
+  // Delete task
+  const deleteTask = (id) => {
+    let newTasks = toDo.filter((task) => task.id !== id);
+    setToDo(newTasks);
+  };
+
+  // Mark task as done or completed
+  const markDone = (id) => {
+    let newTask = toDo.map((task) => {
+      if (task.id === id) {
+        return { ...task, status: !task.status };
+      }
+      return task;
+    });
+    setToDo(newTask);
+  };
+
+  // Cancel update
+  const cancelUpdate = () => {
+    setUpdateData("");
+  };
+
+  // Change task for update
+  const changeTask = (e) => {
+    let newEntry = {
+      id: updateData.id,
+      title: e.target.value,
+      status: updateData.status ? true : false,
+    };
+    setUpdateData(newEntry);
+  };
+
+  // Update task
+  const updateTask = () => {
+    let filterRecords = toDo.filter((task) => task.id !== updateData.id);
+    let updatedObject = [...filterRecords, updateData];
+    setToDo(updatedObject);
+    setUpdateData("");
+  };
+
+  return (
+    <div className="container App">
+      <br />
+      <br />
+      <h2>Abdiawal's ToDo List App</h2>
+      <br />
+      <br />
+
+      {updateData ? (
+        <UpdateForm
+          updateData={updateData}
+          changeTask={changeTask}
+          updateTask={updateTask}
+          cancelUpdate={cancelUpdate}
+        />
+      ) : (
+        <AddTaskForm newTask={newTask} setNewTask={setNewTask} addTask={addTask} />
+      )}
+
+      {/* Display ToDos */}
+      {toDo.length ? (
+        <ToDo toDo={toDo} markDone={markDone} setUpdateData={setUpdateData} deleteTask={deleteTask} />
+      ) : (
+        "No Tasks..."
+      )}
+    </div>
+  );
+}
+
+export default App;
